b5ee4f3e2f5876006c55e8bd990965bf0b90271c
kris <kris979@gmail.com>
2020-03-26 14:16:56 +0000

    moving okta way of graphql to marklogic project (tmp)

--- src/main/java/com/agisoft/marklogic/MarklogicApplication.java
+++ src/main/java/com/agisoft/marklogic/MarklogicApplication.java
@@ -1,10 +1,28 @@
 package com.agisoft.marklogic;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
+@Configuration
 public class MarklogicApplication {
+
+    @Bean
+    public ApplicationRunner init(FoodService foodService) {
+        return args -> {
+            Stream.of("Pizza", "Spam", "Eggs", "Avocado").forEach(name -> {
+                Food food = new Food();
+                food.setName(name);
+                foodService.saveFood(food);
+            });
+            foodService.getFoods().forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MarklogicApplication.class, args);
